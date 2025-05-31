43ce668178b487c02d7d31d23dfbd7910a5a1b9a
alexander1939 <alexcifuentes72818@gmail.com>
2025-05-31 12:33:10 -0600

    estructura y estilos del header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import '../assets/styles/style.css'; // Asegúrate de que la ruta sea correcta
+import '../assets/styles/style.css';
+import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="app-header">
-      <h1>Eventos Chiapas</h1>
+      <div className="logo-container">
+        <img
+          src="https://marketplace.canva.com/EAGFFa377Og/2/0/1600w/canva-logo-fiestas-y-pi%C3%B1atas-divertido-morado-y-rosa-C4CRvUL02EU.jpg"
+          alt="Logo"
+          className="logo"
+        />
+        <a href='#home' onClick={() => navigate('/')} className="app-title">Eventos Chiapas</a>
+      </div>
+
+      <nav className="nav-menu">
+        <a href="#eventos" onClick={() => navigate('/events')} className="nav-link">Eventos</a>
+        <a href="#contacto" onClick={() => navigate('/contact')} className="nav-link">Contacto</a>
+      </nav>
     </header>
   );
 };
