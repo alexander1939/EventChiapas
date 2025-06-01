21ae7fb740cd51d4aaca0f15e1ff742301086c8e
alexander1939 <alexcifuentes72818@gmail.com>
2025-06-01 16:58:10 -0600

    Actualizacion de subir el proyecto en el git pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Contact from './pages/contact';
 import Event from './pages/event'; // Asegúrate que la importación es correcta
@@ -7,13 +7,13 @@ import Layout from './components/Layout';
 
 const AppRouter: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/contact" element={<Layout><Contact /></Layout>} />
         <Route path="/events" element={<Layout><Event /></Layout>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
